478198531b7a05e72ea391ca7fbd1395d861c05e
f-tavin <francois.tavin@iut-dijon.u-bourgogne.fr>
2025-02-04 17:17:40 +0100

    6. Menu et sous-menu - ajout des éléments des sous-menus dans le html - ajout des règles css par défaut pour le sous-menu caché - ajout d'une classe css menu-active pour faire apparaître un sous-menu - ajout d'un script qui ajoute/retire la classe menu-active du sous-menu correspondant quand on passe sur une entrée du menu - gestion du décalage différencié selon la largeur de l'écran

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,17 @@ const logo_droite = document.querySelector('header>img:last-of-type');
 logo_droite.addEventListener('click', ()=> {
     logo_droite.src='logos/UB_old.jpg';
 });
+
+
+/*  TOP MENU  */
+
+const entrees_menu = document.querySelectorAll('menu>li');
+
+entrees_menu.forEach((entree) => {
+    entree.addEventListener('mouseover', (e) => {
+        entree.firstChild.nextSibling.classList.add('menu-active');
+    });
+    entree.addEventListener('mouseout', (e) => {
+        entree.firstChild.nextSibling.classList.remove('menu-active');
+    });
+});
