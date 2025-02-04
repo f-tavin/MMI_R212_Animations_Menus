57158ce3cbeb696c01306219b8abe90b1dd116dd
f-tavin <francois.tavin@iut-dijon.u-bourgogne.fr>
2025-02-04 15:20:47 +0100

    3. Clic sur le logo de droite - ajout d'un script qui change le src du logo de droite au clic

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('menu');
 logo_gauche.addEventListener('click', ()=> {
     menu.classList.toggle('menu-reversed');
 });
+
+
+const logo_droite = document.querySelector('header>img:last-of-type');
+
+logo_droite.addEventListener('click', ()=> {
+    logo_droite.src='logos/UB_old.jpg';
+});
